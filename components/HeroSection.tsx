d18c13b6bf1463fa4d7609a350033641ecf2e503
sherifsani <sanisherif11@gmail.com>
2024-08-27 15:46:21 +0100

    tidy:navbar

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Link from "next/link";
 import { FaArrowRightLong } from "react-icons/fa6";
+import MergedCircles from "./MergedCircles";
 
 const HeroSection = () => {
   return (
-    <section className="p-5">
-      <div className="hero-texts flex flex-col gap-5 items-start">
+    <section className="p-5 pt-8">
+      <div className="hero-texts flex flex-col gap-6 items-start">
         <p className="text-green text-[12px] font-[500] flex items-center gap-2">
           <span className="available-dot w-[6px] h-[6px] bg-green rounded-full"></span>
           Available for work
         </p>
-        <h1 className="hero-title text-white text-4xl">
+        <h1 className="hero-title -mt-4 text-white text-4xl">
           Precision in cloud engineering & software development
         </h1>
         <p className="tagline text-white">
@@ -24,6 +25,13 @@ const HeroSection = () => {
           Work with me
           <FaArrowRightLong className="text-[10px]" />
         </Link>
+        <div className="flex items-center">
+          <MergedCircles />
+          <span className="text-[12px] text-white">
+            Satisfied clients are the
+            <br /> cornerstone of my success
+          </span>
+        </div>
       </div>
     </section>
   );
--- components/MergedCircles.tsx
+++ components/MergedCircles.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const MergedCircles = () => {
   return (
-    <div>MergedCircles</div>
-  )
-}
+    <div className="flex items-center">
+      <div className="w-[32px] h-[32px] rounded-full overflow-clip">
+        <Image
+          src="/assets/client-1.jpeg"
+          width={32}
+          height={32}
+          className="rounded-full h-[32px] w-[32px] object-cover"
+          alt="client"
+        />
+      </div>
+      <div className="w-[32px] h-[32px] rounded-full overflow-clip -translate-x-2">
+        <Image
+          src="/assets/client-2.jpeg"
+          width={32}
+          className="rounded-full h-[32px] w-[32px] object-cover"
+          height={32}
+          alt="client"
+        />
+      </div>
+      <div className="w-[32px] h-[32px] rounded-full overflow-clip -translate-x-4">
+        <Image
+          src="/assets/client-3.jpeg"
+          width={32}
+          className="rounded-full h-[32px] w-[32px] object-cover"
+          height={32}
+          alt="client"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default MergedCircles
+export default MergedCircles;
